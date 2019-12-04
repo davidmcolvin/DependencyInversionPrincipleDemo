2783a0762fa06d0718a1cb06529525ebff352cbb
david colvin <davidmcolvin@gmail.com>
2019-12-03 20:18:08 -0500

    Initial commit of Dependency Inversion Principle Demo

---
+++ DemoLibrary/Emailer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+  public class Emailer
+  {
+    public void SendEmail(Person owner, string v)
+    {
+      Console.WriteLine($"Simulating sending an email to {owner.EmailAddress}");
+    }
+  }
+}
